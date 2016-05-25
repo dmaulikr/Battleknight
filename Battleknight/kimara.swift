c93f74361a2334953fdd8e01516f7651d43ff5a3
Eric Cuevas <ejcue@me.com>
2016-05-25 00:12:38 -0500

    commiting all files

---
+++ Battleknight/kimara.swift
@@ -0,0 +1,9 @@
+//
+//  kimara.swift
+//  Battleknight
+//
+//  Created by Eric Cuevas on 5/16/16.
+//  Copyright © 2016 mackenstein. All rights reserved.
+//
+
+import Foundation
---
+++ Battleknight/player.swift
@@ -0,0 +1,9 @@
+//
+//  player.swift
+//  Battleknight
+//
+//  Created by Eric Cuevas on 5/16/16.
+//  Copyright © 2016 mackenstein. All rights reserved.
+//
+
+import Foundation
